e2f5f39826a69a85e6c855dac51de6707f687315
soma0sd <soma0sd@gmail.com>
2018-07-09 13:26:07 +0900

    API응답제한 대응을 위한 캐시 사용 함수 추가

--- js-dev/jQuery.s0.github.js
+++ js-dev/jQuery.s0.github.js
@@ -1,16 +1,23 @@
-jQuery.githubUser = function(username, callback) {
-  jQuery.getJSON("https://api.github.com/users/" + username + "/repos", callback);
+jQuery.getRepoData = function(username, callback, failcall){
+  jQuery.getJSON("https://api.github.com/users/" + username + "/repos", callback).fail(failcall);
 }
-
 jQuery.fn.loadRepositories = function(username) {
   this.html("<span>저장소를 읽어들이는 중</span>");
 
   var target = this;
-  $.githubUser(username, function(data) {
-    var repos = data;
+  var repos;
+  $.getRepoData(username, function(data){
+    repos = data;
     sortByNumberOfWatchers(repos);
-    console.log(repos);
-
+    localStorage.setItem("repodata", data);
+    var list = $('<dl/>');
+    target.empty().append(list);
+    $(repos).each(function() {
+      list.append('<dt><a href="'+ this.svn_url +'">' + this.name + '</a></dt>');
+      list.append('<dd>' + this.description + '</dd>');
+    });
+  }, function(data){
+    repos = localStorage.getItem("repodata");
     var list = $('<dl/>');
     target.empty().append(list);
     $(repos).each(function() {
@@ -19,8 +26,9 @@ jQuery.fn.loadRepositories = function(username) {
     });
   });
 
+
   function sortByNumberOfWatchers(repos) {
-    repos.sort(function(a,b) {
+    $(repos).sort(function(a,b) {
       return b.watchers - a.watchers;
     });
   }
